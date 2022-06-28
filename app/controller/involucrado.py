0285e2db4e0608b98e40d46a09b8c091ecbc8555
josemarza <josemarzamendia@gmail.com>
2022-06-28 15:02:49 -0400

    cambio de from datetime import datetime

--- app/controller/involucrado.py
+++ app/controller/involucrado.py
@@ -1,7 +1,7 @@
 from flask import (
     Blueprint, render_template,request
 )
-import datetime
+from datetime import datetime
 from app.model import Involucrado
 
 bp = Blueprint('involucrado', __name__)
