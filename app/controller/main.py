625d57a69f4c00446aeb93e5ef9f93ecdce9d2d0
Kamelpy <rinipor008@gmail.com>
2022-06-29 05:31:48 -0400

    cambios en los htmls y rutas en main

--- app/controller/main.py
+++ app/controller/main.py
@@ -11,9 +11,25 @@ def index():
 
 @bp.route('/soluciones')
 def soluciones():
-    return render_template('soluciones.html')
+    return render_template('soluciones2.html')
+    
+@bp.route('/mas-info')
+def mas_info():
+    return render_template('mas-info.html')
 
 @bp.route('/donaciones')
 def donaciones():
-    return render_template("donaciones.html")
+    return render_template("donaciones2.html")
+
+@bp.route('/problematica')
+def problematica():
+    return render_template("problematica.html")
+
+@bp.route('/que-hacer')
+def que_hacer():
+    return render_template("que-hacer.html")
+
+@bp.route('/conocenos')
+def conocenos():
+    return render_template("conocenos.html")
 
